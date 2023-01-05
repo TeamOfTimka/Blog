3ff2792f52aba1c3e824d85bdafada35a9c89e92
TeamOfTimka <lozhkintima112@gmail.com>
2023-01-05 14:27:37 +0300

    created first model and carried out migration

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250) # Название статьи
+    slug = models.SlugField(max_length=250, unique_for_date='publish') # название для формирования URL'ов
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',) # Порядок отображения статей
+
+    def __str__(self):
+        return self.title
