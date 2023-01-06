592e25273cd91fb017e31f645ca465f67c056e26
TeamOfTimka <lozhkintima112@gmail.com>
2023-01-06 13:32:57 +0300

    added ability to send email

--- blogApp/views.py
+++ blogApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 def post_list(request):
     object_list = Post.published.all()
@@ -26,3 +28,23 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blogApp/post/list.html'
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля формы прошли валидацию
+            cd = form.cleaned_data
+            # ... Отправка электронной почты
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], cd['comments'])
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, cd['email'], [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blogApp/post/share.html', {'post': post, 'form': form, 'sent': sent})
